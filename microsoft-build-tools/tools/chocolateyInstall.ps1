7d1aadce1780c81c247aec059cfdfbe28d37d264
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-04-11 15:25:46 +0200

    Chocolatey package for Microsoft Build Tools 2013

---
+++ microsoft-build-tools/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+	'microsoft-build-tools' 'exe' "/Passive /NoRestart /Log $($env:temp)\microsoft-build-tools.log" `
+	'http://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe' -validExitCodes @(0,3010)
