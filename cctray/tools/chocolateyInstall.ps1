2d050a604e3aa04e8610ba36c86bde09070c02f2
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-04-16 11:35:22 +0200

    Chocolatey package for CCTray (CruiseControl.NET client application)

---
+++ cctray/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+	'cctray' 'exe' '/S' `
+	'http://build.nauck-it.de/download/CruiseControl.NET-package/1.8.4.0/CruiseControl.NET-CCTray-1.8.4.0-Setup.exe' -validExitCodes @(0)
