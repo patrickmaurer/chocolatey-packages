c9f001c39048ec534d8317abadd7e90dfc737978
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-04-16 10:59:01 +0200

    Chocolatey package for Windows SDK for Windows 8.1

---
+++ windows-8-1-sdk/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+	'windows-8-1-sdk' 'exe' "/Quiet /NoRestart /Log $($env:temp)\windows-8-1-sdk.log" `
+	'http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe' -validExitCodes @(0,3010)
