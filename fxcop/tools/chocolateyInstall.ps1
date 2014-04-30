94f21f518303dcce3d8008fff8faf32426fb9781
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-04-30 10:47:35 +0200

    Chocolatey package for FxCop (download url found at: https://fxcopinstaller.codeplex.com/)

---
+++ fxcop/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+	'fxcop' 'EXE' '/Q' `
+	'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=fxcopinstaller&DownloadId=821386&FileTime=130407655516000000&Build=20885' -validExitCodes @(0)
