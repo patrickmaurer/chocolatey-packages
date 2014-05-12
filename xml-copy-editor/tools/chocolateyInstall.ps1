c85bbf1dc2643943a91031730e037d0a7ad0f3f8
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-05-12 09:20:22 +0200

    Chocolatey package for XML Copy Editor

---
+++ xml-copy-editor/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+Install-ChocolateyPackage `
+	'xml-copy-editor' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP' `
+	'http://downloads.sourceforge.net/project/xml-copy-editor/xmlcopyeditor-windows/1.2.1.1/xmlcopyeditor-1.2.1.1-install.exe'
