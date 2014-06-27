c4b92920f601491360541449c8410d6a1f6c1443
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-06-27 13:46:47 +0200

    Update CCTray to 1.8.5.0

--- cctray/tools/chocolateyInstall.ps1
+++ cctray/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿Install-ChocolateyPackage `
 	'cctray' 'exe' '/S' `
-	'http://build.nauck-it.de/download/CruiseControl.NET-package/1.8.4.0/CruiseControl.NET-CCTray-1.8.4.0-Setup.exe' -validExitCodes @(0)
+	'http://build.nauck-it.de/download/CruiseControl.NET-package/1.8.5.0/CruiseControl.NET-CCTray-1.8.5.0-Setup.exe' -validExitCodes @(0)
