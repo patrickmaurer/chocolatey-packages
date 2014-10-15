9a009c095f0e48ea9ee4eb221a5b6f60afaae947
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-10-15 14:03:09 +0200

    Updated CrystalReports for VisualStudio to 13 SP11

--- crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
+++ crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
@@ -1,17 +1,17 @@
 ﻿try {
 	$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-	Install-ChocolateyZipPackage 'crystalreports-for-visualstudio' 'http://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_10.exe' "$toolsDir\CRforVS_13_0_10"
+	Install-ChocolateyZipPackage 'crystalreports-for-visualstudio' 'http://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_11.exe' "$toolsDir\CRforVS_13_0_11"
 
 	$sdkFileFullPath = get-childitem $toolsDir -recurse -include CrystalReportsForVisualStudio.msi | select -First 1
-	Install-ChocolateyInstallPackage 'crystalreports-for-visualstudio' 'MSI' "/q /l $($env:temp)\crystalreports-for-visualstudio-sdk.log" "$sdkFileFullPath"
+	Install-ChocolateyInstallPackage 'crystalreports-for-visualstudio' 'MSI' "/q /l $($env:temp)\crystalreports-for-visualstudio-sdk.log UPGRADE=1" "$sdkFileFullPath"
 
 	$is64bit = Get-ProcessorBits 64;
 	if ($is64bit) {
-		$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_10.msi | select -First 1
-		Install-ChocolateyInstallPackage 'crystalreports-for-visualstudio' 'MSI' "/q /l $($env:temp)\crystalreports-for-visualstudio-runtime-x64.log" "$runtimeFileFullPath"
+		$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_11.msi | select -First 1
+		Install-ChocolateyInstallPackage 'crystalreports-for-visualstudio' 'MSI' "/q /l $($env:temp)\crystalreports-for-visualstudio-runtime-x64.log UPGRADE=1" "$runtimeFileFullPath"
 	}
 	
-	Remove-Item "$toolsDir\CRforVS_13_0_10" -recurse
+	Remove-Item "$toolsDir\CRforVS_13_0_11" -recurse
 
 	Write-ChocolateySuccess 'crystalreports-for-visualstudio'
 } catch {
