678343fba0707457577e912523b30bb88a49a468
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-11-01 21:24:52 +0100

    changed package id to lowercase

--- Windows7-and-DotNet3.5SP1-SDK/tools/chocolateyInstall.ps1
+++ Windows7-and-DotNet3.5SP1-SDK/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿try {
 	$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-	Get-ChocolateyWebFile 'Windows7-and-DotNet3.5SP1-SDK' "$toolsDir\GRMSDK_EN_DVD.iso" `
+	Get-ChocolateyWebFile 'windows7-and-dotnet3.5sp1-sdk' "$toolsDir\GRMSDK_EN_DVD.iso" `
 		'http://download.microsoft.com/download/2/E/9/2E911956-F90F-4BFB-8231-E292A7B6F287/GRMSDK_EN_DVD.iso' `
 		'http://download.microsoft.com/download/2/E/9/2E911956-F90F-4BFB-8231-E292A7B6F287/GRMSDKX_EN_DVD.iso' `
 		-checksum '8695F5E6810D84153181695DA78850988A923F4E' -checksumType 'sha1' `
@@ -9,11 +9,11 @@
 	Start-Process "$toolsDir\7z.exe" -ArgumentList "x -o`"$toolsDir\GRMSDK_EN_DVD`" -y `"$toolsDir\GRMSDK_EN_DVD.iso`"" -Wait -WindowStyle Hidden -PassThru
 	Remove-Item "$toolsDir\GRMSDK_EN_DVD.iso"
 
-	Install-ChocolateyInstallPackage 'Windows7-and-DotNet3.5SP1-SDK' 'EXE' "-q -params:ADDLOCAL=ALL" "$toolsDir\GRMSDK_EN_DVD\setup.exe" -validExitCodes @(0,3010)
+	Install-ChocolateyInstallPackage 'windows7-and-dotnet3.5sp1-sdk' 'EXE' "-q -params:ADDLOCAL=ALL" "$toolsDir\GRMSDK_EN_DVD\setup.exe" -validExitCodes @(0,3010)
 	Remove-Item "$toolsDir\GRMSDK_EN_DVD" -recurse
 
-	Write-ChocolateySuccess 'Windows7-and-DotNet3.5SP1-SDK'
+	Write-ChocolateySuccess 'windows7-and-dotnet3.5sp1-sdk'
 } catch {
-	Write-ChocolateyFailure 'Windows7-and-DotNet3.5SP1-SDK' "$($_.Exception.Message)"
+	Write-ChocolateyFailure 'windows7-and-dotnet3.5sp1-sdk' "$($_.Exception.Message)"
 	throw
 }
