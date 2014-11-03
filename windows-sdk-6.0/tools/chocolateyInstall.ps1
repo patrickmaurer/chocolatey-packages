f37aa9d1611d2b40cbbe2f4754ece296e4256542
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2014-11-03 10:27:11 +0100

    Add packages for Windows SDK 6.0 and 6.1

---
+++ windows-sdk-6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+	$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+	Get-ChocolateyWebFile 'windows-sdk-6.0' "$toolsDir\WindowsSDK_Vista_Feb2007Update_rtm.DVD.Rel.iso" `
+		'http://download.microsoft.com/download/4/2/6/42684501-9ec5-43dd-9dfe-c8c9dfa6a66f/6.1.6000.16384.10.WindowsSDK_Vista_Feb2007Update_rtm.DVD.Rel.iso'
+	Start-Process "$toolsDir\7z.exe" -ArgumentList "x -o`"$toolsDir\WinSDK_Vista_rtm`" -y `"$toolsDir\WindowsSDK_Vista_Feb2007Update_rtm.DVD.Rel.iso`"" -Wait -WindowStyle Hidden -PassThru
+	Remove-Item "$toolsDir\WindowsSDK_Vista_Feb2007Update_rtm.DVD.Rel.iso"
+
+	Install-ChocolateyInstallPackage 'windows-sdk-6.0' 'EXE' "-q -params:ADDLOCAL=ALL" "$toolsDir\WinSDK_Vista_rtm\Setup.Exe" -validExitCodes @(0,3010)
+	Remove-Item "$toolsDir\WinSDK_Vista_rtm" -recurse
+
+	Write-ChocolateySuccess 'windows-sdk-6.0'
+} catch {
+	Write-ChocolateyFailure 'windows-sdk-6.0' "$($_.Exception.Message)"
+	throw
+}
---
+++ windows-sdk-6.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+	$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+	Get-ChocolateyWebFile 'windows-sdk-6.1' "$toolsDir\KRMSDK_EN.iso" `
+		'http://download.microsoft.com/download/f/e/6/fe6eb291-e187-4b06-ad78-bb45d066c30f/6.0.6001.18000.367-KRMSDK_EN.iso'
+	Start-Process "$toolsDir\7z.exe" -ArgumentList "x -o`"$toolsDir\KRMSDK_EN`" -y `"$toolsDir\KRMSDK_EN.iso`"" -Wait -WindowStyle Hidden -PassThru
+	Remove-Item "$toolsDir\KRMSDK_EN.iso"
+
+	Install-ChocolateyInstallPackage 'windows-sdk-6.1' 'EXE' "-q -params:ADDLOCAL=ALL" "$toolsDir\KRMSDK_EN\Setup.Exe" -validExitCodes @(0,3010)
+	Remove-Item "$toolsDir\KRMSDK_EN" -recurse
+
+	Write-ChocolateySuccess 'windows-sdk-6.1'
+} catch {
+	Write-ChocolateyFailure 'windows-sdk-6.1' "$($_.Exception.Message)"
+	throw
+}
