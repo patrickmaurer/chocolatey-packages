48e9bc9b454f988d73edfe2a998a76efdf8dc8a3
Patrick <patrickmaurer@users.noreply.github.com>
2014-11-20 22:22:10 +0100

    Remove 7z.exe from packages and depend on 7zip.install package

--- windows-sdk-7.0/tools/chocolateyInstall.ps1
+++ windows-sdk-7.0/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@
 		'http://download.microsoft.com/download/2/E/9/2E911956-F90F-4BFB-8231-E292A7B6F287/GRMSDKX_EN_DVD.iso' `
 		-checksum '8695F5E6810D84153181695DA78850988A923F4E' -checksumType 'sha1' `
 		-checksum64 '3393C98B8468CB3505557854922707510F8B65E1' -checksumType64 'sha1'
-	Start-Process "$toolsDir\7z.exe" -ArgumentList "x -o`"$toolsDir\GRMSDK_EN_DVD`" -y `"$toolsDir\GRMSDK_EN_DVD.iso`"" -Wait -WindowStyle Hidden -PassThru
+	Start-Process "${Env:ProgramFiles}\7-Zip\7z.exe" -ArgumentList "x -o`"$toolsDir\GRMSDK_EN_DVD`" -y `"$toolsDir\GRMSDK_EN_DVD.iso`"" -Wait -WindowStyle Hidden -PassThru
 	Remove-Item "$toolsDir\GRMSDK_EN_DVD.iso"
 
 	Install-ChocolateyInstallPackage 'windows-sdk-7.0' 'EXE' "-q -params:ADDLOCAL=ALL" "$toolsDir\GRMSDK_EN_DVD\setup.exe" -validExitCodes @(0,3010)
--- windows-sdk-7.1/tools/chocolateyInstall.ps1
+++ windows-sdk-7.1/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@
 		'http://download.microsoft.com/download/F/1/0/F10113F5-B750-4969-A255-274341AC6BCE/GRMSDKX_EN_DVD.iso' `
 		-checksum 'CDE254E83677C34C8FD509D6B733C32002FE3572' -checksumType 'sha1' `
 		-checksum64 '9203529F5F70D556A60C37F118A95214E6D10B5A' -checksumType64 'sha1'
-	Start-Process "$toolsDir\7z.exe" -ArgumentList "x -o`"$toolsDir\GRMSDK_EN_DVD`" -y `"$toolsDir\GRMSDK_EN_DVD.iso`"" -Wait -WindowStyle Hidden -PassThru
+	Start-Process "${Env:ProgramFiles}\7-Zip\7z.exe" -ArgumentList "x -o`"$toolsDir\GRMSDK_EN_DVD`" -y `"$toolsDir\GRMSDK_EN_DVD.iso`"" -Wait -WindowStyle Hidden -PassThru
 	Remove-Item "$toolsDir\GRMSDK_EN_DVD.iso"
 
 	Install-ChocolateyInstallPackage 'windows-sdk-7.1' 'EXE' "-q -params:ADDLOCAL=ALL" "$toolsDir\GRMSDK_EN_DVD\setup.exe" -validExitCodes @(0,3010)
