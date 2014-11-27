7cdbfaea60d6e3babd177ea3d0d0195e304e055e
Patrick <patrickmaurer@users.noreply.github.com>
2014-11-27 22:46:26 +0100

    Package for Visual Studio 2010 Express C#

---
+++ VisualStudio2010ExpressCSharp/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'VisualStudio2010ExpressCSharp'
+$installerType = 'EXE'
+$32BitUrl = 'http://download.microsoft.com/download/4/9/3/493D82B1-363D-4954-A87D-C4B7C5A1C895/vcs_web.exe'
+$silentArgs = "/q /REBOOT=ReallySuppress"
+$validExitCodes = @(0,3010)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
