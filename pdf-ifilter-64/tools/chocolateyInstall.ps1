9baf52ed4b535927c9c9635fdf2d7e4767db50f3
Patrick <patrickmaurer@users.noreply.github.com>
2014-11-29 11:37:19 +0100

    Better naming of URL variable for future versions

--- pdf-ifilter-64/tools/chocolateyInstall.ps1
+++ pdf-ifilter-64/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'pdf-ifilter-64'
 $installerType = 'MSI'
-$32BitUrl = 'http://ardownload.adobe.com/pub/adobe/acrobat/win/11.x/PDFFilter64Setup.msi'
+$url = 'http://ardownload.adobe.com/pub/adobe/acrobat/win/11.x/PDFFilter64Setup.msi'
 $silentArgs = "/Quiet /NoRestart /Log $($env:temp)\pdf-ifilter-64.log"
 $validExitCodes = @(0,3010)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
