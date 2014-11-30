239700cfd44ab7db893fd411f873e582a8547ee6
Patrick <patrickmaurer@users.noreply.github.com>
2014-11-30 18:31:18 +0100

    Add bypass for unnecessary Visual Studio 2010 check and set correct version number

--- netfx-4.0.1-devpack/tools/chocolateyInstall.ps1
+++ netfx-4.0.1-devpack/tools/chocolateyInstall.ps1
@@ -3,4 +3,9 @@ $installerType = 'EXE'
 $32BitUrl = 'http://download.microsoft.com/download/A/A/F/AAF243D8-C5C2-4632-AF96-CB65F378CE7D/VS10SP1-KB2495593.exe'
 $silentArgs = "/Quiet /NoRestart /Log $($env:temp)\netfx-4.0.1-devpack.log"
 $validExitCodes = @(0,3010)
+
+#Installer aborts if VS2010 (full edition) is not installed even though it's not necessary. Add necessary values to bypass this check.
+New-Item -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing' -Name '10.0' –Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0' -Name 'SP' -Value 1 -PropertyType 'DWord' -Force
+
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
