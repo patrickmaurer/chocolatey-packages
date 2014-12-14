89077baa8785829c121ef959ca57262b431d6637
Patrick <patrickmaurer@users.noreply.github.com>
2014-12-14 14:26:38 +0100

    Package for Android Debug Bridge

---
+++ adb/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'adb'
+$url = 'https://dl-ssl.google.com/android/repository/platform-tools_r21-windows.zip'
+$checksum = '04b26e60e47cda4867d321817270058c22572352'
+$checksumType = 'sha1'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+try {
+	Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
+
+	Remove-Item "$unzipLocation\platform-tools\api" -recurse
+	Remove-Item "$unzipLocation\platform-tools\systrace" -recurse
+	Remove-Item "$unzipLocation\platform-tools\dmtracedump.exe"
+	Remove-Item "$unzipLocation\platform-tools\etc1tool.exe"
+	Remove-Item "$unzipLocation\platform-tools\hprof-conv.exe"
+	Remove-Item "$unzipLocation\platform-tools\source.properties"
+	Remove-Item "$unzipLocation\platform-tools\sqlite3.exe"
+
+	Write-ChocolateySuccess 'adb'
+} catch {
+	Write-ChocolateyFailure 'adb' "$($_.Exception.Message)"
+	throw
+}
