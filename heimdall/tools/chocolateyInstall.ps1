353d0a0428551f1172304d65f359a17197942cce
Patrick <patrickmaurer@users.noreply.github.com>
2014-12-15 22:14:09 +0100

    Package for Heimdall Suite

---
+++ heimdall/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'heimdall'
+$url = 'https://bitbucket.org/benjamin_dobell/heimdall/downloads/heimdall-suite-1.4.0-win32.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+Install-ChocolateyDesktopLink -TargetFilePath "$unzipLocation\Heimdall Suite\heimdall-frontend.exe"
