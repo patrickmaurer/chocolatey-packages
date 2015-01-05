bf4dd7c2a79480649cb3cc88c71d44322a416047
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2015-01-05 13:52:14 +0100

    Package for Citrix ShareFile sync (submitted nupkg)

---
+++ citrix-sharefile-sync/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'citrix-sharefile-sync'
+$installerType = 'MSI'
+$url = 'https://www.citrix.com/downloads/sharefile/clients-and-plug-ins/sharefile-sync-for-windows-32.html'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Write-Debug "Downloading main download page from '$url'"
+$wc = New-Object System.Net.WebClient
+$wc.Proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$mainPage = $wc.DownloadString($url)
+
+if ($mainPage -match '"(//downloads.citrix.com/7110/ShareFileSync32_3.2.109.0.msi\?.*?)"') {
+	$32BitUrl = 'https:' + $Matches[1]
+	Write-Debug "32 bit download URL is '$32BitUrl'"
+}
+
+if ($mainPage -match '"(//downloads.citrix.com/7110/ShareFileSync64_3.2.109.0.msi\?.*?)"') {
+	$64BitUrl = 'https:' + $Matches[1]
+	Write-Debug "64 bit download URL is '$64BitUrl'"
+}
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
---
+++ citrix-sharefile-sync/tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+﻿$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -match "Citrix ShareFile Sync" -and ($_.Version -eq "3.2.109.0") }
+$app.Uninstall()
