145abb3f1f4956c6e7f49095f31c6a43babc3408
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2015-01-05 13:53:38 +0100

    Add exception handling (not re-submitted, for future releases)

--- citrix-sharefile-sync/tools/chocolateyUninstall.ps1
+++ citrix-sharefile-sync/tools/chocolateyUninstall.ps1
@@ -1,2 +1,9 @@
-﻿$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -match "Citrix ShareFile Sync" -and ($_.Version -eq "3.2.109.0") }
-$app.Uninstall()
+﻿$packageName = 'citrix-sharefile-sync'
+try {
+	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -match "Citrix ShareFile Sync" -and ($_.Version -eq "3.2.109.0") }
+	$app.Uninstall()
+	Write-ChocolateySuccess "$packageName"
+} catch {
+	Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
+	throw
+}
