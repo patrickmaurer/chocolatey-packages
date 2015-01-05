5024a83d9a3b1e0c1a5b6f330176648697f2f1fe
Patrick <patrickmaurer@users.noreply.github.com>
2015-01-05 21:10:36 +0100

    Updated and improved Boxcryptor Classic package

---
+++ BoxCryptorClassic/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'BoxCryptorClassic'
+try {
+	Uninstall-ChocolateyPackage "$packageName" 'MSI' '{DF47AB90-FB92-42F4-926E-1C4FF16029E7} /quiet /norestart' -validExitCodes @(0,3010)
+	Write-ChocolateySuccess "$packageName"
+} catch {
+	Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
+	throw
+}
