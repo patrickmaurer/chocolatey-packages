bc5b746ae22e23e74467e37c30f72f8b0d874cc3
Patrick <patrickmaurer@users.noreply.github.com>
2015-01-06 19:43:53 +0100

    Updates as per moderator request

--- boxcryptor/tools/chocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'boxcryptor'
-try {
-	Uninstall-ChocolateyPackage "$packageName" 'MSI' '{0511514A-A672-4F79-8151-D70CA84BF044} /quiet /norestart' -validExitCodes @(0,3010)
-	Write-ChocolateySuccess "$packageName"
-} catch {
-	Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
-	throw
-}
