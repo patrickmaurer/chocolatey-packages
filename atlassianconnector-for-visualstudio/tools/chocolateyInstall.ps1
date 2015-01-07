67ffaecde05ed061378156dd6187ab53a5d91f6e
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2015-01-07 08:44:44 +0100

    Atlassian Connector for Visual Studio package

---
+++ atlassianconnector-for-visualstudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'atlassianconnector-for-visualstudio'
+$installerType = 'EXE'
+$32BitUrl = 'http://update.atlassian.com/atlassian-vs-plugin/Downloads/atlassian-vs-connector-setup-1.3.11-STABLE-20130903-1404.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
