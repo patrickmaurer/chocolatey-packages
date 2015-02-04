f0990f88df36349c5d4fc324c1b7ee36923b1f89
Patrick <patrickmaurer@users.noreply.github.com>
2015-02-04 21:20:54 +0100

    Update to 4.0.10.0

--- msbuild.extensionpack/tools/chocolateyInstall.ps1
+++ msbuild.extensionpack/tools/chocolateyInstall.ps1
@@ -1,20 +1,28 @@
+$packageName = 'msbuild.extensionpack'
+$installerType = 'MSI'
+$url = 'https://github.com/mikefourie/MSBuildExtensionPack/releases/download/July2014/MSBuild.Extension.Pack.July.2014.zip'
+$silentArgs = '/Quiet'
+$validExitCodes = @(0)
+
 try {
 	$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-	Install-ChocolateyZipPackage 'msbuild.extensionpack' 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msbuildextensionpack&DownloadId=808874&FileTime=130381716453930000&Build=20885' $toolsDir
+	Install-ChocolateyZipPackage "$packageName" "$url" $toolsDir
+	Get-ChocolateyUnzip "$toolsDir\MSBuild.Extension.Pack.July.2014\MSBuild Extension Pack 4.0.10.0.zip" $toolsDir
 
 	$is64bit = Get-ProcessorBits 64;
 	$x86FileFullPath = Get-ChildItem $toolsDir -recurse -include *x86*.msi | select -First 1
 	$x64FileFullPath = Get-ChildItem $toolsDir -recurse -include *x64*.msi | select -First 1
 
-	Install-ChocolateyInstallPackage 'msbuild.extensionpack' 'MSI' '/Quiet' "$x86FileFullPath"
+	Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$x86FileFullPath" -validExitCodes $validExitCodes
 	if ($is64bit) {
-		Install-ChocolateyInstallPackage 'msbuild.extensionpack' 'MSI' '/Quiet' "$x64FileFullPath"
+		Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$x64FileFullPath" -validExitCodes $validExitCodes
 	}
 
-	Remove-Item "$toolsDir\4.0.9.0" -recurse
+	Remove-Item "$toolsDir\MSBuild.Extension.Pack.July.2014" -recurse
+	Remove-Item "$toolsDir\MSBuild Extension Pack 4.0.10.0" -recurse
 
-	Write-ChocolateySuccess 'msbuild.extensionpack'
+	Write-ChocolateySuccess "$packageName"
 } catch {
-	Write-ChocolateyFailure 'msbuild.extensionpack' "$($_.Exception.Message)"
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
 	throw
 }
