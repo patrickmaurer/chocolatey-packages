e660fb410eb4f840cea7c7dfa7040c153f3facf0
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2015-02-10 16:57:58 +0100

    Package for Tcptunnel

---
+++ tcptunnel/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'tcptunnel'
+$url = 'http://www.vakuumverpackt.de/tcptunnel/tcptunnel-0.8.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+try {
+	Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+	Move-Item "$unzipLocation\tcptunnel-0.8\*" $unzipLocation
+	Remove-Item "$unzipLocation\tcptunnel-0.8" -recurse
+
+	Write-ChocolateySuccess 'tcptunnel'
+} catch {
+	Write-ChocolateyFailure 'tcptunnel' "$($_.Exception.Message)"
+	throw
+}
