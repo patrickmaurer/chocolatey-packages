f0e84939ba57a78f07eefad31833150e8570691a
Patrick <patrickmaurer@users.noreply.github.com>
2015-02-22 15:15:02 +0100

    Update to version 1.2.1.3

--- xml-copy-editor/tools/chocolateyInstall.ps1
+++ xml-copy-editor/tools/chocolateyInstall.ps1
@@ -1,3 +1,8 @@
-Install-ChocolateyPackage `
-	'xml-copy-editor' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP' `
-	'http://downloads.sourceforge.net/project/xml-copy-editor/xmlcopyeditor-windows/1.2.1.1/xmlcopyeditor-1.2.1.1-install.exe'
+$packageName = 'xml-copy-editor'
+$installerType = 'EXE'
+$32BitUrl = 'http://downloads.sourceforge.net/project/xml-copy-editor/xmlcopyeditor-windows/1.2.1.3/xmlcopyeditor-1.2.1.3-x86-install.exe'
+$64BitUrl = 'http://downloads.sourceforge.net/project/xml-copy-editor/xmlcopyeditor-windows/1.2.1.3/xmlcopyeditor-1.2.1.3-x64-install.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
