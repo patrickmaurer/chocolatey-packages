ef2b3500eeb764ac1bed1381d2ce1baa4d40a152
Patrick <patrickmaurer@users.noreply.github.com>
2015-02-22 16:55:42 +0100

    Package for EchoTool

---
+++ echotool/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'echotool'
+$url = 'http://bansky.net/echotool/echotool.exe'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$targetPath = Join-Path $toolsDir 'echotool.exe'
+
+Get-ChocolateyWebFile $packageName $targetPath $url
