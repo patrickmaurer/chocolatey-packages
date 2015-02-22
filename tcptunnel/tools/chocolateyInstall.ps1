57e4ba0ea436497f9c9517006b168a2f2949472f
Patrick <patrickmaurer@users.noreply.github.com>
2015-02-22 15:30:29 +0100

    Do not move files after unzip

--- tcptunnel/tools/chocolateyInstall.ps1
+++ tcptunnel/tools/chocolateyInstall.ps1
@@ -2,14 +2,4 @@
 $url = 'http://www.vakuumverpackt.de/tcptunnel/tcptunnel-0.8.zip'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-try {
-	Install-ChocolateyZipPackage $packageName $url $unzipLocation
-
-	Move-Item "$unzipLocation\tcptunnel-0.8\*" $unzipLocation
-	Remove-Item "$unzipLocation\tcptunnel-0.8" -recurse
-
-	Write-ChocolateySuccess 'tcptunnel'
-} catch {
-	Write-ChocolateyFailure 'tcptunnel' "$($_.Exception.Message)"
-	throw
-}
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
