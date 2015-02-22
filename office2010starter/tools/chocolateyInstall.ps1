dd992730c5d78d974c2d5fcc6806c6c59482673f
Patrick <patrickmaurer@users.noreply.github.com>
2015-02-22 15:16:19 +0100

    Package for Office 2010 Starter

---
+++ office2010starter/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'office2010starter'
+$installerType = 'EXE'
+$32BitUrl = 'http://files01.techspot.com/temp/setupconsumerc2rolw.exe'
+$silentArgs = ''
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
