fbc5a65025f243cc5474b2e44c71b2be402beb56
Patrick <patrickmaurer@users.noreply.github.com>
2015-04-15 21:53:53 +0200

    Change download URL of cctray to SourceForge (build.nauck-it.de unavailable)

--- cctray/tools/chocolateyInstall.ps1
+++ cctray/tools/chocolateyInstall.ps1
@@ -1,3 +1,7 @@
-﻿Install-ChocolateyPackage `
-	'cctray' 'exe' '/S' `
-	'http://build.nauck-it.de/download/CruiseControl.NET-package/1.8.5.0/CruiseControl.NET-CCTray-1.8.5.0-Setup.exe' -validExitCodes @(0)
+﻿$packageName = 'cctray'
+$installerType = 'EXE'
+$url = 'http://downloads.sourceforge.net/project/ccnet/CruiseControl.NET%20Releases/CruiseControl.NET%201.8.5/CruiseControl.NET-CCTray-1.8.5.0-Setup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
