72f296e77395d88b8bc8c8c450bbce2d86c34274
Patrick <patrickmaurer@users.noreply.github.com>
2015-08-19 22:01:48 +0200

    Add .NET 4.6 Multi Targeting Pack

---
+++ netfx-4.6-devpack/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'netfx-4.6-devpack'
+$installerType = 'EXE'
+$url = 'http://download.microsoft.com/download/8/2/F/82FF2034-83E6-4F93-900D-F88C7AD9F3EE/NDP46-TargetingPack-KB3045566.exe'
+$silentArgs = "/Quiet /NoRestart /Log $($env:temp)\netfx-4.6-devpack.log"
+$validExitCodes = @(0,3010)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
