c1b0c4ed68130d2e6e30773600dbb0c22b2a66a5
Patrick <patrickmaurer@users.noreply.github.com>
2015-08-24 21:16:34 +0200

    Warn if adb.exe or fastboot.exe already exists outside choco

--- adb/tools/chocolateyInstall.ps1
+++ adb/tools/chocolateyInstall.ps1
@@ -4,6 +4,16 @@ $checksum = '52b4816990d2d18a34645bbf20a31b38a7859d4c'
 $checksumType = 'sha1'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
+$existingAdb = Get-Command -Name adb.exe -ErrorAction SilentlyContinue
+if (($existingAdb -ne $null) -and ($existingAdb.Path.StartsWith($env:ChocolateyInstall) -eq $false)) {
+	Write-Warning "adb.exe already exists: $($existingAdb.Path)"
+}
+
+$existingFastboot = Get-Command -Name fastboot.exe -ErrorAction SilentlyContinue
+if (($existingFastboot -ne $null) -and ($existingFastboot.Path.StartsWith($env:ChocolateyInstall) -eq $false)) {
+	Write-Warning "fastboot.exe already exists: $($existingFastboot.Path)"
+}
+
 Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
 
 Remove-Item "$unzipLocation\platform-tools\api" -recurse
