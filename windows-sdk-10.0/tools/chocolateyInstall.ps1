5207c2f270258fb97fef4a75b64d0edcf69a77db
Patrick <patrickmaurer@users.noreply.github.com>
2015-09-06 14:36:16 +0200

    Better path for log files

--- windows-sdk-10.0/tools/chocolateyInstall.ps1
+++ windows-sdk-10.0/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'windows-sdk-10.0'
 $installerType = 'EXE'
 $url = 'http://download.microsoft.com/download/E/1/F/E1F1E61E-F3C6-4420-A916-FB7C47FBC89E/standalonesdk/sdksetup.exe'
-$silentArgs = "/Quiet /NoRestart /Log $($env:temp)\windows-sdk-8.1.log"
+$silentArgs = "/Quiet /NoRestart /Log ""$env:temp\$packageName.log"""
 $validExitCodes = @(0,3010)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
