37318f107d4d62bba3a73fedcfa050cfb1dc0d7e
Patrick <patrickmaurer@users.noreply.github.com>
2015-09-30 22:28:07 +0200

    Cleanup after install, also in case of errors

--- netfx-4.0.1-devpack/tools/chocolateyInstall.ps1
+++ netfx-4.0.1-devpack/tools/chocolateyInstall.ps1
@@ -1,11 +1,36 @@
 ﻿$packageName = 'netfx-4.0.1-devpack'
 $installerType = 'EXE'
 $32BitUrl = 'http://download.microsoft.com/download/A/A/F/AAF243D8-C5C2-4632-AF96-CB65F378CE7D/VS10SP1-KB2495593.exe'
-$silentArgs = "/Quiet /NoRestart /Log $($env:temp)\netfx-4.0.1-devpack.log"
+$silentArgs = "/Quiet /NoRestart /Log ""$($env:temp)\netfx-4.0.1-devpack.log"""
 $validExitCodes = @(0,3010)
 
 #Installer aborts if VS2010 (full edition) is not installed even though it's not necessary. Add necessary values to bypass this check.
-New-Item -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing' -Name '10.0' –Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0' -Name 'SP' -Value 1 -PropertyType 'DWord' -Force
+$keyAlreadyExists = Test-Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0'
+if ($keyAlreadyExists -eq $false) {
+	Write-Debug 'Creating temporary key: "HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0"'
+	New-Item -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing' -Name '10.0' –Force
+}
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+try {
+	$key = Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0'
+	$valueAlreadyExists = $key.GetValue('SP', $null) -ne $null
+
+	if ($valueAlreadyExists -eq $false) {
+		Write-Debug 'Creating temporary value: "HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0[@SP=1]"'
+		New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0' -Name 'SP' -Value 1 -PropertyType 'DWord' -Force
+	}
+
+	try {
+		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+	} finally {
+		if ($valueAlreadyExists -eq $false) {
+			Write-Debug 'Removing temporary value: "HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0[@SP=1]"'
+			Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0' -Name 'SP' -Force
+		}
+	}
+} finally {
+	if ($keyAlreadyExists -eq $false) {
+		Write-Debug 'Removing temporary key: "HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0"'
+		Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0' –Force
+	}
+}
