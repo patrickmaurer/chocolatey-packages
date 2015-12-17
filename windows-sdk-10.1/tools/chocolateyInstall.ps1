f08d04f0e43f86b4840667497ac52c2c13b34bfe
Jakub Bereżański <kuba@berezanscy.pl>
2015-12-17 23:22:17 +0100

    add windows-sdk-10.1 (Windows 10 SDK November release)

---
+++ windows-sdk-10.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'windows-sdk-10.1'
+$installerType = 'EXE'
+$url = 'https://download.microsoft.com/download/6/A/2/6A2ECE81-C934-4E47-91CC-52DA00A65345/sdksetup.exe'
+$silentArgs = "/Quiet /NoRestart /Log ""$env:temp\$packageName.log"""
+$validExitCodes = @(0,3010)
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
