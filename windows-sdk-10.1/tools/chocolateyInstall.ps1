754eb71273f0388e60ae1331589e757b6c690c04
Jakub Bereżański <kuba@berezanscy.pl>
2015-12-18 00:12:29 +0100

    windows-sdk-10.1: do not overwrite previous log file
    Using a static log file name makes each installation attempt overwrite the
    log file from the previous attempt. This is inconvenient when
    troubleshooting installation issues on a particular system: it is often
    beneficial to be able to compare log files from subsequent installation
    attempts.
    
    Make the log file name unique by appending a guid generated for each
    installation attempt.
    
    Note that this also covers the update scenario.

--- windows-sdk-10.1/tools/chocolateyInstall.ps1
+++ windows-sdk-10.1/tools/chocolateyInstall.ps1
@@ -3,6 +3,6 @@ $installerType = 'EXE'
 $url = 'https://download.microsoft.com/download/6/A/2/6A2ECE81-C934-4E47-91CC-52DA00A65345/sdksetup.exe'
 $checksum = 'D1C0AA957F15661F00CFA633E9DAFB39A26D6557'
 $checksumType = 'sha1'
-$silentArgs = "/Quiet /NoRestart /Log ""$env:temp\$packageName.log"""
+$silentArgs = "/Quiet /NoRestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
 $validExitCodes = @(0,3010)
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
