8cd216a88fc7cfe2bdfd2230495ffba54b62f0e6
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2016-04-21 16:32:07 +0200

    Update Crystal Reports to SP 16

--- crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
+++ crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
@@ -1,16 +1,22 @@
-﻿$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+﻿$packageName = 'crystalreports-for-visualstudio'
+$installerType = 'MSI'
+$url = 'http://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_16.exe'
+$silentArgsSDK = "/q /l ""$env:temp\$packageName-SDK.log"" UPGRADE=1"
+$silentArgsRuntime = "/q /l ""$env:temp\$packageName-Runtime.log"" UPGRADE=1"
 
-Get-ChocolateyWebFile 'crystalreports-for-visualstudio' "$toolsDir\CRforVS_13_0_14.exe" 'http://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_14.exe'
-Get-ChocolateyUnzip "$toolsDir\CRforVS_13_0_14.exe" "$toolsDir\CRforVS_13_0_14"
-Remove-Item "$toolsDir\CRforVS_13_0_14.exe"
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Get-ChocolateyWebFile 'crystalreports-for-visualstudio' "$toolsDir\CRforVS_13_0_16.exe" "$url"
+Get-ChocolateyUnzip "$toolsDir\CRforVS_13_0_16.exe" "$toolsDir\CRforVS_13_0_16"
+Remove-Item "$toolsDir\CRforVS_13_0_16.exe"
 
 $sdkFileFullPath = get-childitem $toolsDir -recurse -include CrystalReportsForVisualStudio.msi | select -First 1
-Install-ChocolateyInstallPackage 'crystalreports-for-visualstudio' 'MSI' "/q /l $($env:temp)\crystalreports-for-visualstudio-sdk.log UPGRADE=1" "$sdkFileFullPath"
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgsSDK" "$sdkFileFullPath"
 
 $is64bit = Get-ProcessorBits 64;
 if ($is64bit) {
-	$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_14.msi | select -First 1
-	Install-ChocolateyInstallPackage 'crystalreports-for-visualstudio' 'MSI' "/q /l $($env:temp)\crystalreports-for-visualstudio-runtime-x64.log UPGRADE=1" "$runtimeFileFullPath"
+	$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_16.msi | select -First 1
+	Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgsRuntime" "$runtimeFileFullPath"
 }
 
-Remove-Item "$toolsDir\CRforVS_13_0_14" -recurse
+Remove-Item "$toolsDir\CRforVS_13_0_16" -recurse
