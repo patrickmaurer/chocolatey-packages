8354895eb01eb9d2bc99681ac6160410444be4bd
Patrick <patrickmaurer@users.noreply.github.com>
2016-04-28 22:01:41 +0200

    Update boxcryptor to V2.2.423.322

--- boxcryptor/tools/chocolateyInstall.ps1
+++ boxcryptor/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'boxcryptor'
 $installerType = 'MSI'
-$32BitUrl = 'https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_v2.2.411.228_Setup.msi'
-$silentArgs = '/quiet /norestart'
+$url = 'https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_v2.2.423.322_Setup.msi'
+$silentArgs = "/Quiet /NoRestart /Log ""$($env:temp)\boxcryptor-2.2.423.322-install.log"""
 $validExitCodes = @(0,3010)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
