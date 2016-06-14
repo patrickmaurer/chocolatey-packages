cf74cb39dbaeafd3e358eb20f52c439baec5f666
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2016-06-14 12:52:50 +0200

    Update 2

--- microsoft-build-tools/tools/chocolateyInstall.ps1
+++ microsoft-build-tools/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'microsoft-build-tools'
 $installerType = 'EXE'
-$url = 'http://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe'
-$silentArgs = "/Passive /NoRestart /Log $($env:temp)\microsoft-build-tools-2015.log"
+$url = 'http://download.microsoft.com/download/1/f/e/1fe5d584-c5a9-4aee-907a-421abe4c74f2/buildtools_full.exe'
+$silentArgs = "/Passive /NoRestart /Log ""$($env:temp)\microsoft-build-tools-2015.log"""
 $validExitCodes = @(0,3010)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
