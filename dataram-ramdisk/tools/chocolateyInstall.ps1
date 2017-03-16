81dc559e1ef4ade307bce42396eae3e9d3674003
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2017-03-16 15:36:38 +0100

    Upgrade to RC36

--- dataram-ramdisk/tools/chocolateyInstall.ps1
+++ dataram-ramdisk/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
 $packageName = 'dataram-ramdisk'
 $installerType = 'MSI'
-$url = 'http://memory.dataram.com/__downloads/memory/ramdisk/Dataram_RAMDisk_4_4_0_RC34.msi'
+$url = 'http://memory.dataram.com/__downloads/memory/ramdisk/Dataram_RAMDisk_4_4_0_RC36.msi'
+$checksum = 'EA7906A346AA437BD0F3C056F084D6BA2184F945A9F1993671FBC2D32A735636'
+$checksumType = 'sha256'
 $silentArgs = '/quiet /norestart'
 $validExitCodes = @(0,3010)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$checksum" -checksumType "$checksumType" -validExitCodes $validExitCodes
