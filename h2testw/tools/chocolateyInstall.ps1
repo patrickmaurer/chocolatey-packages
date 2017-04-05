596006339c97c1ef50ff3b19f04420ccf55d828f
Patrick <patrickmaurer@users.noreply.github.com>
2017-04-05 22:32:46 +0200

    Package for h2testw (from c't magazine)

---
+++ h2testw/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'h2testw'
+$url = 'ftp://ftp.heise.de/pub/ct/ctsi/h2testw_1.4.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+Install-ChocolateyShortcut -shortcutFilePath "$($env:ProgramData)\Microsoft\Windows\Start Menu\H2testw.lnk" -targetPath "$unzipLocation\h2testw.exe"
