87e38d73253833545359c512537283b19b1545f0
Patrick <patrickmaurer@users.noreply.github.com>
2017-08-09 22:02:50 +0200

    Windows SDK 10.1 August 2016

--- windows-sdk-10.1/tools/chocolateyInstall.ps1
+++ windows-sdk-10.1/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'windows-sdk-10.1'
 $installerType = 'EXE'
-$url = 'https://download.microsoft.com/download/6/A/2/6A2ECE81-C934-4E47-91CC-52DA00A65345/sdksetup.exe'
-$checksum = 'D1C0AA957F15661F00CFA633E9DAFB39A26D6557'
+$url = 'https://download.microsoft.com/download/6/3/B/63BADCE0-F2E6-44BD-B2F9-60F5F073038E/standalonesdk/SDKSETUP.EXE'
+$checksum = 'D9D88A2C3C8C196E06A948613B4C749ADDBAEF04'
 $checksumType = 'sha1'
 $silentArgs = "/Quiet /NoRestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
 $validExitCodes = @(0,3010)
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -checksum $checksum -checksumType $checksumType -validExitCodes $validExitCodes
