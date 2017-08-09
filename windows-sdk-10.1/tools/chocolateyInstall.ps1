e018f51944c1ab92c1b94e88fa2e904021075f47
Patrick <patrickmaurer@users.noreply.github.com>
2017-08-09 23:08:20 +0200

    Windows SDK for .NET 4.7 (10.1.15063.468)

--- windows-sdk-10.1/tools/chocolateyInstall.ps1
+++ windows-sdk-10.1/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'windows-sdk-10.1'
 $installerType = 'EXE'
-$url = 'https://download.microsoft.com/download/6/3/B/63BADCE0-F2E6-44BD-B2F9-60F5F073038E/standalonesdk/SDKSETUP.EXE'
-$checksum = 'D9D88A2C3C8C196E06A948613B4C749ADDBAEF04'
+$url = 'https://download.microsoft.com/download/E/1/B/E1B0E6C0-2FA2-4A1B-B322-714A5586BE63/windowssdk/winsdksetup.exe'
+$checksum = '56ADAA1B131D819693B93060D3D14506C246FECD'
 $checksumType = 'sha1'
 $silentArgs = "/Quiet /NoRestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
 $validExitCodes = @(0,3010)
