a95949351846f927e36185ebc308e86e7b2db1ec
Patrick <patrickmaurer@users.noreply.github.com>
2017-11-02 22:56:32 +0100

    Update to 4.1.4, introduce package parameter for locale

--- OpenOffice/checksums/checksums.ps1
+++ OpenOffice/checksums/checksums.ps1
@@ -1,9 +1,7 @@
-﻿#$url = 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.3/binaries/en-US/Apache_OpenOffice_4.1.3_Win_x86_install_en-US.exe'
-$url = 'http://netix.dl.sourceforge.net/project/openofficeorg.mirror/4.1.3/binaries/en-US/Apache_OpenOffice_4.1.3_Win_x86_install_en-US.exe'
+﻿#$url = 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.4/binaries/en-US/Apache_OpenOffice_4.1.4_Win_x86_install_en-US.exe'
+$url = 'http://netix.dl.sourceforge.net/project/openofficeorg.mirror/4.1.4/binaries/en-US/Apache_OpenOffice_4.1.4_Win_x86_install_en-US.exe'
 
-$locales = @(
-#'el', 'en-GB', 'en-US', 'es', 'eu', 'ca', 'ca-XR', 'ca-XV', 'cs', 'ru', 'zh-CN', 'zh-TW', 'vi', 'ta', 'th', 'tr', 'sk', 'sl', 'sr', 'sv', 'pl', 'pt', 'pt-BR', 'nb', 'nl', 'lt', 'km', 'ko', 'ja', 'it',
- 'he', 'hi', 'hu', 'gd', 'gl', 'fi', 'fr', 'da', 'de', 'bg', 'ast')
+$locales = @('el', 'en-GB', 'en-US', 'es', 'eu', 'ca', 'ca-XR', 'ca-XV', 'cs', 'ru', 'zh-CN', 'zh-TW', 'vi', 'ta', 'th', 'tr', 'sk', 'sl', 'sr', 'sv', 'pl', 'pt', 'pt-BR', 'nb', 'nl', 'lt', 'km', 'ko', 'ja', 'it', 'he', 'hi', 'hu', 'gd', 'gl', 'fi', 'fr', 'da', 'de', 'bg', 'ast')
 
 foreach ($locale in $locales) {
 	$localeUrl = $url -replace 'en-US', $locale
