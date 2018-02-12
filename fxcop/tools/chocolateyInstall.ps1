ec3c7ea6bfacf0b41e98a724f39c7e9db7224bd5
Patrick <patrickmaurer@users.noreply.github.com>
2018-02-12 21:06:40 +0100

    Download FxCop from Codeplex archive

--- fxcop/tools/chocolateyInstall.ps1
+++ fxcop/tools/chocolateyInstall.ps1
@@ -1,3 +1,22 @@
-﻿Install-ChocolateyPackage `
-	'fxcop' 'EXE' '/Q' `
-	'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=fxcopinstaller&DownloadId=821386&FileTime=130407655516000000&Build=20885' -validExitCodes @(0)
+﻿$packageName = 'fxcop'
+$installerType = 'EXE'
+$url = 'https://codeplexarchive.blob.core.windows.net/archive/projects/FxCopInstaller/FxCopInstaller.zip'
+$checksum = 'F157404789C1B722345BDE4553A98F302A3B6F624F0CBFE2B0A1A666F7BBE2A5'
+$checksumType = 'sha256'
+$silentArgs = '/Q'
+$validExitCodes = @(0)
+
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+Install-ChocolateyInstallPackage $packageName $installerType $silentArgs "$unzipLocation\sourceCode\fxcopinstaller\WinSDK_FxCopSetup.exe" `
+	-checksum $checksum `
+	-checksumType $checksumType `
+	-validExitCodes $validExitCodes
+
+Remove-Item "$toolsDir\discussions" -recurse
+Remove-Item "$toolsDir\issues" -recurse
+Remove-Item "$toolsDir\license" -recurse
+Remove-Item "$toolsDir\releases" -recurse
+Remove-Item "$toolsDir\sourceCode" -recurse
+Remove-Item "$toolsDir\wiki" -recurse
