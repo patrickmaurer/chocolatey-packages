4c12d30ea02e2484626f334b1a8a2c779a9eac9d
Patrick <patrickmaurer@users.noreply.github.com>
2018-05-01 22:33:59 +0200

    Update BoxCryptor classic download URL

--- BoxCryptorClassic/tools/chocolateyInstall.ps1
+++ BoxCryptorClassic/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
 ﻿$packageName = 'BoxCryptorClassic'
 $installerType = 'MSI'
-$32BitUrl = 'https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_Classic_v1.7.409.131_Setup.msi'
-$silentArgs = '/quiet /norestart'
+$url = 'https://downloads.boxcryptor.com/Boxcryptor_Classic_v1.7.409.131_Setup.msi'
+$checksum = '02B381A27F8B11C7F05AFC859C0FA9B260EC764E52CFB7600C94F4964C97A11F'
+$checksumType = 'sha256'
+$silentArgs = "/Quiet /NoRestart /Log ""$($env:temp)\boxcryptor-classic-v1.7.409.131-install.log"""
 $validExitCodes = @(0,3010)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$checksum" -checksumType "$checksumType" -validExitCodes $validExitCodes
