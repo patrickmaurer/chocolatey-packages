2283ee909ba1bfb7d11a661fd103e191f2b9dd54
Patrick <patrickmaurer@users.noreply.github.com>
2018-05-14 20:44:43 +0200

    Update

--- boxcryptor/tools/chocolateyInstall.ps1
+++ boxcryptor/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'boxcryptor'
 $installerType = 'MSI'
-$url = 'https://downloads.boxcryptor.com//boxcryptor/windows/Boxcryptor_v2.24.747_Setup.msi'
-$checksum = '84BACA896DAAFA23611323719DF0188C634545EA0E3816586150ABAA641B4D79'
+$url = 'https://downloads.boxcryptor.com//boxcryptor/windows/Boxcryptor_v2.28.797_Setup.msi'
+$checksum = '87FA8AE44BD8CF8A5FFF1A57917972B9DB4B15195FF8F15D01B07974B1268B50'
 $checksumType = 'sha256'
-$silentArgs = "/Quiet /NoRestart /Log ""$($env:temp)\boxcryptor-2.24.747-install.log"""
+$silentArgs = "/Quiet /NoRestart /Log ""$($env:temp)\boxcryptor-2.28.797-install.log"""
 $validExitCodes = @(0,3010)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$checksum" -checksumType "$checksumType" -validExitCodes $validExitCodes
