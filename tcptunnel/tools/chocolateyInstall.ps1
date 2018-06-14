2ae6672e4620f9ba988e3ad52b546ac75a9e0f17
Patrick <patrickmaurer@users.noreply.github.com>
2018-06-14 22:30:03 +0200

    Add checksum

--- tcptunnel/tools/chocolateyInstall.ps1
+++ tcptunnel/tools/chocolateyInstall.ps1
@@ -1,5 +1,10 @@
 ﻿$packageName = 'tcptunnel'
 $url = 'http://www.vakuumverpackt.de/tcptunnel/tcptunnel-0.8.zip'
+$checksum = 'FF3E6346D179D9AA6877AF087A557F68EBE3440B4CF23EB920FF476E186A8005'
+$checksumType = 'sha256'
+
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
+Install-ChocolateyZipPackage $packageName $url $unzipLocation `
+	-checksum $checksum `
+	-checksumType $checksumType
