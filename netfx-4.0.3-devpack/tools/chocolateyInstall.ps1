bd1d12c08742decc2dcbec93a6d1454029699ee5
Patrick <patrickmaurer@users.noreply.github.com>
2018-06-14 23:05:39 +0200

    Add checksum

--- netfx-4.0.3-devpack/tools/chocolateyInstall.ps1
+++ netfx-4.0.3-devpack/tools/chocolateyInstall.ps1
@@ -1,6 +1,9 @@
 ﻿$packageName = 'netfx-4.0.3-devpack'
 $installerType = 'EXE'
-$32BitUrl = 'http://download.microsoft.com/download/3/B/2/3B229A81-1413-45C6-9BFE-259D90606FD6/netfx_403mtpack.exe'
+$url = 'https://download.microsoft.com/download/3/B/2/3B229A81-1413-45C6-9BFE-259D90606FD6/netfx_403mtpack.exe'
+$checksum = 'D8BA2E61E2F0E5D39668BB0564BAE4EA4E2AC82108525B3B3307F3C1EE8F8295'
+$checksumType = 'sha256'
 $silentArgs = "/Quiet /NoRestart /Log $($env:temp)\netfx-4.0.3-devpack.log"
 $validExitCodes = @(0,3010)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$checksum" -checksumType "$checksumType" -validExitCodes $validExitCodes
