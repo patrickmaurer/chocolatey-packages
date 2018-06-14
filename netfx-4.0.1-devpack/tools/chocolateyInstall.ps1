8b744b0c5834a0092e4e10e3966622ad782833c8
Patrick <patrickmaurer@users.noreply.github.com>
2018-06-14 22:59:49 +0200

    Add checksum

--- netfx-4.0.1-devpack/tools/chocolateyInstall.ps1
+++ netfx-4.0.1-devpack/tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
 ﻿$packageName = 'netfx-4.0.1-devpack'
 $installerType = 'EXE'
-$32BitUrl = 'http://download.microsoft.com/download/A/A/F/AAF243D8-C5C2-4632-AF96-CB65F378CE7D/VS10SP1-KB2495593.exe'
+$url = 'https://download.microsoft.com/download/A/A/F/AAF243D8-C5C2-4632-AF96-CB65F378CE7D/VS10SP1-KB2495593.exe'
+$checksum = '39E1FFD6A4AF90670A5AF89CEFB9C7FA4325FBE7FFE8773FA9F7800B160E6D72'
+$checksumType = 'sha256'
 $silentArgs = "/Quiet /NoRestart /Log ""$($env:temp)\netfx-4.0.1-devpack.log"""
 $validExitCodes = @(0,3010)
 
@@ -21,7 +23,7 @@ try {
 	}
 
 	try {
-		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$checksum" -checksumType "$checksumType" -validExitCodes $validExitCodes
 	} finally {
 		if ($valueAlreadyExists -eq $false) {
 			Write-Debug 'Removing temporary value: "HKLM:\SOFTWARE\Microsoft\DevDiv\VS\Servicing\10.0[@SP=1]"'
