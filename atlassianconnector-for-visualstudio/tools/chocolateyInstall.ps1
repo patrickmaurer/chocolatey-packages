625636ebda5053aeec735770761268cebc3c1d4c
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2018-07-02 13:04:10 +0200

    Fix download URL and add checksum

--- atlassianconnector-for-visualstudio/tools/chocolateyInstall.ps1
+++ atlassianconnector-for-visualstudio/tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
 ﻿$packageName = 'atlassianconnector-for-visualstudio'
 $installerType = 'EXE'
-$32BitUrl = 'http://update.atlassian.com/atlassian-vs-plugin/Downloads/atlassian-vs-connector-setup-1.3.12-STABLE-20150410-0907.exe'
+$url = 'https://marketplace-cdn.atlassian.com/files/artifact/da1f2597-1c20-447f-8a64-c2f26defa266/atlassian-vs-connector-setup-1.3.12-STABLE-20150410-0907.exe'
+$checksum = '245118EDF905EE30D429BE63D42F4B4A03114DA6891EF049152EF5AF38784E6C'
+$checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
@@ -12,4 +14,4 @@ if ($vs2008Installed -eq $false -and $vs2010Installed -eq $false -and $vs2012Ins
 	throw 'Required version of Visual Studio is not installed (2008, 2010, 2012, or 2013).'
 }
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$checksum" -checksumType "$checksumType" -validExitCodes $validExitCodes
