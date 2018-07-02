bc2fc58f5c7fc88076afad557bb2003490a1e793
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2018-07-02 13:15:35 +0200

    Add checksum

--- h2testw/tools/chocolateyInstall.ps1
+++ h2testw/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName = 'h2testw'
 $url = 'ftp://ftp.heise.de/pub/ct/ctsi/h2testw_1.4.zip'
-$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$checksum = '0D54B8BECA3BC2EBFAB543DBE793D52658D278538F9062B816FB77ABE2DAC187'
+$checksumType = 'sha256'
 
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum "$checksum" -checksumType "$checksumType"
 Install-ChocolateyShortcut -shortcutFilePath "$($env:ProgramData)\Microsoft\Windows\Start Menu\H2testw.lnk" -targetPath "$unzipLocation\h2testw.exe"
