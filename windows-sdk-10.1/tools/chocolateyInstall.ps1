a4cf753a29b9ce0a6623e00da9f241d08f9632b6
Patrick <patrickmaurer@users.noreply.github.com>
2018-07-05 22:07:47 +0200

    Update to 1803 version

--- windows-sdk-10.1/tools/chocolateyInstall.ps1
+++ windows-sdk-10.1/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'windows-sdk-10.1'
 $installerType = 'EXE'
-$url = 'https://download.microsoft.com/download/E/1/B/E1B0E6C0-2FA2-4A1B-B322-714A5586BE63/windowssdk/winsdksetup.exe'
-$checksum = '56ADAA1B131D819693B93060D3D14506C246FECD'
-$checksumType = 'sha1'
+$url = 'https://download.microsoft.com/download/5/A/0/5A08CEF4-3EC9-494A-9578-AB687E716C12/windowssdk/winsdksetup.exe'
+$checksum = '93C9DCA3A9F28061A601F3E6B737DEDE40C2C77CFD200ED5CB6EFE2AB0C9D5CC'
+$checksumType = 'sha256'
 $silentArgs = "/Quiet /NoRestart /Log ""$env:temp\${packageName}_$([Guid]::NewGuid().ToString('D')).log"""
 $validExitCodes = @(0,3010)
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url -checksum $checksum -checksumType $checksumType -validExitCodes $validExitCodes
