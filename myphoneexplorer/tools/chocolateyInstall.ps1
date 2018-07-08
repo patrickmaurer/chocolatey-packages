dd020048477a6777e0974c1e2cce813942c9138c
Patrick <patrickmaurer@users.noreply.github.com>
2018-07-08 12:27:41 +0200

    Update version

--- myphoneexplorer/tools/chocolateyInstall.ps1
+++ myphoneexplorer/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'myphoneexplorer'
 $installerType = 'EXE'
-$32BitUrl = 'http://www.fjsoft.at/files/MyPhoneExplorer_Setup_1.8.8.exe'
-$checksum = '9af3ce782a459063c15959918dcda6ba5cf813c212ef8c5a2607d26b8b1a9e5d'
-$checksumType = 'sha256'
+$32BitUrl = 'https://www.fjsoft.at/files/MyPhoneExplorer_Setup_v1.8.9.exe'
+$checksum = 'a056f6c1c0e94fff37e46ebefb61496820bf5248'
+$checksumType = 'sha1'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
