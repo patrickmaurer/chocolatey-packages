46ba4012d369a21fbcaa2d1191f5f755afc313db
Patrick Maurer <patrickmaurer@users.noreply.github.com>
2018-12-11 16:59:05 +0100

    Update version

--- OpenOffice/checksums/checksums.ps1
+++ OpenOffice/checksums/checksums.ps1
@@ -1,17 +1,17 @@
-﻿#$url = 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.5/binaries/en-US/Apache_OpenOffice_4.1.5_Win_x86_install_en-US.exe'
-$url = 'http://netix.dl.sourceforge.net/project/openofficeorg.mirror/4.1.5/binaries/en-US/Apache_OpenOffice_4.1.5_Win_x86_install_en-US.exe'
+﻿#$url = 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.6/binaries/en-US/Apache_OpenOffice_4.1.6_Win_x86_install_en-US.exe'
+$url = 'http://freefr.dl.sourceforge.net/project/openofficeorg.mirror/4.1.6/binaries/en-US/Apache_OpenOffice_4.1.6_Win_x86_install_en-US.exe'
 
-$locales = @('el', 'en-GB', 'en-US', 'es', 'eu', 'ca', 'ca-XR', 'ca-XV', 'cs', 'ru', 'zh-CN', 'zh-TW', 'vi', 'ta', 'th', 'tr', 'sk', 'sl', 'sr', 'sv', 'pl', 'pt', 'pt-BR', 'nb', 'nl', 'lt', 'km', 'ko', 'ja', 'it', 'he', 'hi', 'hu', 'gd', 'gl', 'fi', 'fr', 'da', 'de', 'bg', 'ast')
+$locales = @('ast', 'bg', 'ca', 'ca-XR', 'ca-XV', 'cs', 'da', 'de', 'el', 'en-GB', 'en-US', 'es', 'eu', 'fi', 'fr', 'gd', 'gl', 'he', 'hi', 'hu', 'it', 'ja', 'km', 'ko', 'lt', 'nb', 'nl', 'pl', 'pt', 'pt-BR', 'ru', 'sk', 'sl', 'sr', 'sv', 'ta', 'th', 'tr', 'vi', 'zh-CN', 'zh-TW')
 
 foreach ($locale in $locales) {
 	$localeUrl = $url -replace 'en-US', $locale
-	$outFile = "D:\Temp\OOO\$locale\setup.exe"
-	
+	$outFile = "C:\Temp\OOO\$locale-setup.exe"
+
 	echo "downloading: $localeUrl"
 	echo "to file:     $outFile"
 
 	New-Item -ItemType Directory -Force -Path "D:\Temp\OOO\$locale"
-	
+
 	Invoke-WebRequest -Uri $localeUrl -OutFile $outFile
 	C:\ProgramData\chocolatey\lib\chocolatey\tools\chocolateyInstall\tools\checksum.exe "$outFile" --type sha256
 }
