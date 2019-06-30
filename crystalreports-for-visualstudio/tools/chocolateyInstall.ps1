a36a95e14575ccf3b5a7f9fdb4de0107d0ab5a49
Patrick <patrickmaurer@users.noreply.github.com>
2019-06-30 23:14:50 +0200

    Update version

--- crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
+++ crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
@@ -1,23 +1,23 @@
 ﻿$packageName = 'crystalreports-for-visualstudio'
 $installerType = 'MSI'
-$url = 'https://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_24.exe'
-$checksum = '2244E354509A62B2404B98CBBFF68335083AA241851EC695C182E73E5B0DFCF1'
+$url = 'https://origin.softwaredownloads.sap.com/public/file/0020000000898892019'
+$checksum = 'EB42DC69E85AF8CF137E2B7A76E3F1F93463A6175B472B17DE3212251F731C89'
 $silentArgsSDK = "/q /l ""$env:temp\$packageName-SDK.log"" UPGRADE=1"
 $silentArgsRuntime = "/q /l ""$env:temp\$packageName-Runtime.log"" UPGRADE=1"
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-Get-ChocolateyWebFile 'crystalreports-for-visualstudio' "$toolsDir\CRforVS_13_0_24.exe" "$url" -Checksum "$checksum" -ChecksumType 'sha256'
-Get-ChocolateyUnzip "$toolsDir\CRforVS_13_0_24.exe" "$toolsDir\CRforVS_13_0_24"
-Remove-Item "$toolsDir\CRforVS_13_0_24.exe"
+Get-ChocolateyWebFile 'crystalreports-for-visualstudio' "$toolsDir\CRforVS_13_0_25.exe" "$url" -Checksum "$checksum" -ChecksumType 'sha256'
+Get-ChocolateyUnzip "$toolsDir\CRforVS_13_0_25.exe" "$toolsDir\CRforVS_13_0_25"
+Remove-Item "$toolsDir\CRforVS_13_0_25.exe"
 
 $sdkFileFullPath = get-childitem $toolsDir -recurse -include CrystalReportsForVisualStudio.msi | select -First 1
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgsSDK" "$sdkFileFullPath"
 
 $is64bit = Get-ProcessorBits 64;
 if ($is64bit) {
-	$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_24.msi | select -First 1
+	$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_25.msi | select -First 1
 	Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgsRuntime" "$runtimeFileFullPath"
 }
 
-Remove-Item "$toolsDir\CRforVS_13_0_24" -recurse
+Remove-Item "$toolsDir\CRforVS_13_0_25" -recurse
