2c92590ad6d6b1415fdc3da2b182c8918520d4f5
Patrick <patrickmaurer@users.noreply.github.com>
2020-05-19 07:06:04 +0200

    checksum changed

--- myphoneexplorer/tools/chocolateyInstall.ps1
+++ myphoneexplorer/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'myphoneexplorer'
 $installerType = 'EXE'
 $32BitUrl = 'https://www.fjsoft.at/files/MyPhoneExplorer_Setup_1.8.14.exe'
-$checksum = 'ec57a80e5c41e232370324c884ddf7eac5400281'
+$checksum = '8eb9beb55fb7080b5c9fa1b3c4a7cc9fe8cf89a9'
 $checksumType = 'sha1'
 $silentArgs = '/S'
 $validExitCodes = @(0)
