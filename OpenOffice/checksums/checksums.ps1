c6f7b1172db52feffdddcd23b719bfa0e2fe2757
Patrick <patrickmaurer@users.noreply.github.com>
2020-11-17 21:25:34 +0100

    update version

--- OpenOffice/checksums/checksums.ps1
+++ OpenOffice/checksums/checksums.ps1
@@ -1,11 +1,10 @@
-﻿#$url = 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.7/binaries/en-US/Apache_OpenOffice_4.1.7_Win_x86_install_en-US.exe'
-$url = 'http://freefr.dl.sourceforge.net/project/openofficeorg.mirror/4.1.7/binaries/en-US/Apache_OpenOffice_4.1.7_Win_x86_install_en-US.exe'
+﻿$url = 'https://master.dl.sourceforge.net/project/openofficeorg.mirror/4.1.8/binaries/en-US/Apache_OpenOffice_4.1.8_Win_x86_install_en-US.exe'
 
 $locales = @('ast', 'bg', 'ca', 'ca-XR', 'ca-XV', 'cs', 'da', 'de', 'el', 'en-GB', 'en-US', 'es', 'eu', 'fi', 'fr', 'gd', 'gl', 'he', 'hi', 'hu', 'it', 'ja', 'km', 'ko', 'lt', 'nb', 'nl', 'pl', 'pt', 'pt-BR', 'ru', 'sk', 'sl', 'sr', 'sv', 'ta', 'th', 'tr', 'vi', 'zh-CN', 'zh-TW')
 
 foreach ($locale in $locales) {
 	$localeUrl = $url -replace 'en-US', $locale
-	$outFile = "C:\Temp\OOO\$locale-setup.exe"
+	$outFile = "D:\Temp\OOO\$locale\setup.exe"
 
 	echo "downloading: $localeUrl"
 	echo "to file:     $outFile"
